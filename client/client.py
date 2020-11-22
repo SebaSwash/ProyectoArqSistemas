07588a48c7f03a7fbfb85229d1cbd6e63c52a812
SebastianSw4sh <sebastian.toro1@mail.udp.cl>
2020-11-22 00:48:38 -0300

    avances autenticación usuarios 21.11

--- client/client.py
+++ client/client.py
@@ -11,10 +11,16 @@
 # ╚═══════════════════════════════════════════════════════════════════════╝
 
 # Módulos a utilizar
-import socket, argparse
+import socket, argparse, os
+from getpass import getpass
+from terminaltables import AsciiTable
+
+# Función para limpiar pantalla
+def clear_screen():
+  os.system('cls' if os.name == 'nt' else 'clear')
 
 # Constantes con nombres de servicios a utilizar
-
+USER_AUTH_SERVICE_NAME = 'uauth'
 
 class Client:
   def __init__(self, host, port):
@@ -59,38 +65,100 @@ class Client:
     tx_data = tx[12:] # Data recibida desde el servidor
 
     return (tx_length, tx_service, tx_status, tx_data)
+
+  # Método para menú y opciones internas de cada servicio
+  def internal_menu_options(self, user_data):
+    # Se genera una tabla con los datos del usuario para mostrarlos en la terminal
+    table_data = [
+      ['Tus datos personales'],
+      ['Nombre: '+str(user_data['user_name'])],
+      ['RUT: '+str(user_data['user_rut'])],
+      ['Email: '+str(user_data['email'])],
+      ['Dirección: '+str(user_data['address'])],
+      ['Tipo de credencial: '+str(user_data['user_type'])]
+    ]
+
+    # Se instancia el objeto de la tabla con los datos generados y se imprime en la terminal
+    table = AsciiTable(table_data)
+    print(table.table)
+
+    # Se muestran a continuación las siguientes opciones de servicios
+    input('ENTER para continuar')
   
-  def run(self):
-    # Ciclo de ejecución del proceso cliente
-    while True:
-      service_name = input('Servicio a invocar: ')
-      msg = input('Mensaje: ')
+  # Método para menú y opción de autentificación
+  def user_menu_options(self):
+    clear_screen()
+    menu_table = '''
+    ┌───────────────────────────────┐
+    │ Bienvenido a My Pet's Friend! │
+    ├───────────────────────────────┤
+    │ [1] Iniciar sesión            │
+    │ [2] Salir                     │
+    └───────────────────────────────┘'''
+    print(menu_table)
+    user_option = input('Selecciona una de las opciones [1-2]: ')
 
-      # Se envía el mensaje al servicio por medio del bus de servicios
-      self.sock.send(self.generate_tx(service_name,msg).encode(encoding='UTF-8'))
+    while user_option not in ['1','2']:
+      user_option = input('Selecciona una de las opciones [1-2]: ')
+    
+    user_option = int(user_option) 
 
-      # Se recibe la transacción desde el servicio
-      tx = self.sock.recv(4096)
+    if user_option == 1:
+      # Se inicia el proceso de autenticación del usuario
+      print('\n')
+      user_rut = input('Ingresa tu RUT sin guión ni puntos: ')
+      password = getpass('Ingresa tu contraseña: ') # getpass permite ocultar la password en la terminal.
 
-      if not tx:
-        # Se cierra el servicio si no se reciben datos desde el socket
-        self.sock.close()
-        break
+      # Se genera el diccionario con los datos para realizar la autenticación en el servicio.
+      user_data = {'user_rut': user_rut, 'password': password, 'tx_option': 1} # 'tx_option' permite al servidor reconocer la tx de autentificación obtenida.
+
+      # Se genera la transacción (tx) en base al objeto creado
+      tx = self.generate_tx(USER_AUTH_SERVICE_NAME, str(user_data)) # El objeto es tomado en formato string.
+
+      # Se envía la transacción al servicio por medio del bus de servicios.
+      self.sock.send(tx.encode(encoding='UTF-8'))
+
+      # Se obtiene el resultado de la transacción desde el servicio
+      # En caso de éxito, se reciben los datos personales del usuario y en caso de error, se obtiene una notificación de error.
+      recv_tx = self.sock.recv(4096)
 
       try:
-        tx = tx.decode('UTF-8')
-        tx_length, tx_service, tx_status, tx_data = self.split_recv_tx(tx)
-        print('------------------------------------------------------------------------------')
-        print('Transacción recibida desde el servicio')
-        print('\t- Largo de la transacción: ' +str(tx_length)+' ('+str(int(tx_length))+')')
-        print('\t- Servicio invocado: '+str(tx_service))
-        print('\t- Estado de transacción: '+str(tx_status))
-        print('\t- Datos recibidos: '+str(tx_data))
+        recv_tx = recv_tx.decode('UTF-8')
+        tx_length, tx_service, tx_status, tx_data = self.split_recv_tx(recv_tx)
+
+        # Se verifica el estado de la transacción ('OK' o 'NK') según el bus de servicios.
+        if tx_status.lower() == 'nk':
+          # Se ha producido un error en la transferencia de la transacción
+          print('[Error] Se ha producido un error en la transferencia de la transacción (NK).')
+        
+        else: # La transacción fue recibida correctamente
+          # Se verifica si se pudo autenticar al usuario
+          user_data = eval(tx_data) # Se transforma el objeto como string a diccionario nuevamente
+          if not user_data['auth_error']:
+            # El usuario fue autenticado correctamente
+            # Se muestra el siguiente menú interno con los otros servicios
+            print(user_data)
+          
+          else:
+            # Hubo un error al autentificar al usuario. Se muestra el mensaje de error enviado por el servicio.
+            print('[Error] El servicio de autenticación ha respondido con el siguiente error:')
+            print(user_data['error_notification'])
       
       except Exception as error:
         print('[Error] Se ha producido el siguiente error al procesar la transacción:')
         print(str(error))
 
+    elif user_option == 2:
+      print('\nHasta luego!')
+      self.sock.close()
+      exit()
+    
+    input('\nPresiona ENTER para continuar')
+
+  def run(self):
+    # Ciclo de ejecución del proceso cliente
+    while True:
+      self.user_menu_options()
 
 if __name__ == '__main__':
   # Configuración de argumentos solicitados al momento de ejecutar el comando en la terminal
---
+++ services/db/db_credentials.py
@@ -0,0 +1,6 @@
+# Archivo contenedor de credenciales de la base de datos
+DB_HOST = 'sql10.freemysqlhosting.net'
+DB_PORT = 3306
+DB_USER = 'sql10377827'
+DB_DATABASE = 'sql10377827'
+DB_PASSWD = 'KmDv2I1qfa'
