8a6f5c5a99770d9c78d0181665d6f3293c017505
SebastianSw4sh <sebastian.toro1@mail.udp.cl>
2020-12-01 13:23:55 -0300

    avances hasta 01.12

--- client/client.py
+++ client/client.py
@@ -11,16 +11,32 @@
 # ╚═══════════════════════════════════════════════════════════════════════╝
 
 # Módulos a utilizar
-import socket, argparse, os
+from consolemenu import *
+from consolemenu.items import *
+
 from getpass import getpass
+import socket, argparse, os, pickle
 from terminaltables import AsciiTable
+from colorama import Back, Fore, Style, init
+from prettytable import PrettyTable
+
+# Inicialización para librería de colores
+init()
 
 # Función para limpiar pantalla
 def clear_screen():
   os.system('cls' if os.name == 'nt' else 'clear')
 
-# Constantes con nombres de servicios a utilizar
-USER_AUTH_SERVICE_NAME = 'uauth'
+# Constantes con nombres de servicios a utilizar (06 corresponde al número de grupo de proyecto)
+USER_AUTH_SERVICE_NAME = 'uas06'
+USER_MANAGEMENTE_SERVICE_NAME = 'ums06'
+
+# Constantes para combinaciones de colores y estilos (Back -> color de fondo, Fore -> color de texto)
+INSTRUCTIONS_STYLE = Back.WHITE + Fore.BLACK
+ERROR_STYLE = Back.RED + Fore.WHITE
+WARNING_STYLE = Back.YELLOW + Fore.WHITE
+INFO_STYLE = Back.BLUE + Fore.WHITE
+SUCCESS_STYLE = Back.GREEN + Fore.WHITE
 
 class Client:
   def __init__(self, host, port):
@@ -37,8 +53,8 @@ class Client:
       self.run() # Inicio de la ejecución del proceso cliente
     
     except Exception as error:
-      print('[Error] Se ha producido el siguiente error al establecer conexión con el bus de servicios:')
-      print(str(error))
+      print(ERROR_STYLE+'[Error] Se ha producido el siguiente error al establecer conexión con el bus de servicios:')
+      print(str(error)+Style.RESET_ALL)
   
   # Método para generar el largo de la transacción (servicio + data) - Ej: 18 (int) --> 00018 (str)
   def generate_tx_length(self, tx_length):
@@ -69,41 +85,138 @@ class Client:
   # Método para menú y opciones internas de cada servicio
   def internal_menu_options(self, user_data):
     # Se genera una tabla con los datos del usuario para mostrarlos en la terminal
-    table_data = [
-      ['Tus datos personales'],
-      ['Nombre: '+str(user_data['user_name'])],
-      ['RUT: '+str(user_data['user_rut'])],
-      ['Email: '+str(user_data['email'])],
-      ['Dirección: '+str(user_data['address'])],
-      ['Tipo de credencial: '+str(user_data['user_type'])]
-    ]
+    #table_data = [
+    #  ['Tus datos personales'],
+    #  ['Nombre: '+str(user_data['nombres'])+' '+str(user_data['apellidos'])],
+    #  ['RUT: '+str(user_data['rut'])],
+    #  ['Email: '+str(user_data['email'])],
+    #  ['Dirección: '+str(user_data['direccion'])],
+    #  ['Tipo de credencial: '+str(user_data['tipo_usuario'])]
+    #]
 
     # Se instancia el objeto de la tabla con los datos generados y se imprime en la terminal
-    table = AsciiTable(table_data)
-    print(table.table)
+    #table = AsciiTable(table_data)
+    #print(table.table)
 
-    # Se muestran a continuación las siguientes opciones de servicios
-    input('ENTER para continuar')
+    clear_screen()
+    menu = ConsoleMenu('Hola nuevamente, '+str(user_data['nombres'])+' '+str(user_data['apellidos']), 'Selecciona una de las opciones a continuación.')
+    menu.append_item(SelectionItem('Sección de usuarios',0))
+    menu.append_item(SelectionItem('Sección de mascotas',1))
+    menu.append_item(SelectionItem('Sección de revisiones de mascotas',2))
+    menu.append_item(SelectionItem('Cerrar sesión',3))
+    menu.show(False) # False evita que se muestra la opción de 'exit' que viene por defecto
+
+    user_option = menu.selected_option
+
+    if user_option == 0: # Sección de usuarios
+      # Se envía la solicitud al servicio de usuarios para obtener el menú interno
+      data = {'tx_option': 0} # La opción 0 solicita al servicio el retorno del menú principal
+      tx = self.generate_tx(USER_MANAGEMENTE_SERVICE_NAME, str(data))
+
+      # Se envía la transacción para obtener el menú interno de la gestión de usuarios
+      self.sock.send(tx.encode(encoding='UTF-8'))
+
+      try:
+        recv_tx = self.sock.recv(4096).decode('UTF-8')
+        tx_length, tx_service, tx_status, tx_data = self.split_recv_tx(recv_tx)
+
+        # Se verifica el estado de la transacción ('OK' o 'NK') según el bus de servicios.
+        if tx_status.lower() == 'nk':
+          # Se ha producido un error en la transferencia de la transacción
+          print(Style.RESET_ALL)
+          print(ERROR_STYLE+'[Error] Se ha producido un error en la transferencia de la transacción (NK).'+Style.RESET_ALL)
+        
+        tx_data = eval(tx_data)
+        # Se genera el menú a partir de las opciones recibidas desde el servicio
+        menu_options = tx_data['menu_options']
+
+        # Se instancia el objeto del menú
+        menu = ConsoleMenu(tx_data['menu_title'], tx_data['menu_subtitle'])
+        index = 0
+        for option in menu_options:
+          menu.append_item(SelectionItem(option, index))
+          index += 1
+        
+        menu.show(False)
+        user_option = menu.selected_option
+
+        if user_option == 0:
+          # Se selecciona la opción de ver lista de usuarios registrados
+
+          # Se genera la transacción para obtener desde el servicio la lista de usuarios
+          data = {'tx_option': 1}
+
+          tx = self.generate_tx(USER_MANAGEMENTE_SERVICE_NAME, str(data))
+
+          # Se envía la solicitud al servicio y se verifica si se envía devuelta la lista o una notificación de error
+          self.sock.send(tx.encode(encoding='UTF-8'))
+
+          recv_tx = self.sock.recv(4096)
+          # Se procesa la respuesta recibida desde el servicio a través del bus
+          tx_length, tx_service, tx_status, tx_data = self.split_recv_tx(recv_tx)
+
+          # Se verifica el estado de la transacción ('OK' o 'NK') según el bus de servicios.
+          if tx_status.lower() == 'nk':
+            # Se ha producido un error en la transferencia de la transacción
+            print(Style.RESET_ALL)
+            print(ERROR_STYLE+'[Error] Se ha producido un error en la transferencia de la transacción (NK).'+Style.RESET_ALL)
+          
+          # Se transforma el diccionario recibido y se procesa
+          try:
+            data = eval(tx_data.decode('UTF-8'))
+            users_list = data['users_list']
+
+            if len(users_list) != 0:
+              # Se genera la tabla con la lista de usuarios obtenidas
+              users_table = PrettyTable()
+
+              # Se agregan las columnas, según los atributos recibidos
+              users_table.field_names = list(users_list[0].keys())
+              
+              # Se agregan las filas en la tabla según cada usuario registrado
+              for user in users_list:
+                # Se transforma el diccionario en una lista con los valores de la información del usuario
+                users_table.add_row(list(user.values()))
+              
+              # Se imprime en la terminal la tabla generada
+              print(INSTRUCTIONS_STYLE+'================================= Lista de usuarios registrados'+Style.RESET_ALL)
+              print(users_table)
+          
+          except Exception as error:
+            print(ERROR_STYLE+'[Error] Se ha producido el siguiente error al procesar la información recibida:')
+            print(str(error)+Style.RESET_ALL)
+
+        # Se muestra el menú generado
+        input('')
+        clear_screen()
+        menu.show(False)
+      
+      except Exception as error:
+        print(ERROR_STYLE+'[Error] Se ha producido el siguiente error al desplegar el menú interno:')
+        print(str(error)+Style.RESET_ALL)
+
+      pass
+
+    elif user_option == 1: # Sección de mascotas
+      return
+
+    elif user_option == 2: # Sección de revisiones de mascotas
+      return
+
+    elif user_option == 3: # Cerrar sesión
+      return
   
   # Método para menú y opción de autentificación
   def user_menu_options(self):
     clear_screen()
-    menu_table = '''
-    ┌───────────────────────────────┐
-    │ Bienvenido a My Pet's Friend! │
-    ├───────────────────────────────┤
-    │ [1] Iniciar sesión            │
-    │ [2] Salir                     │
-    └───────────────────────────────┘'''
-    print(menu_table)
-    user_option = input('Selecciona una de las opciones [1-2]: ')
+    menu = ConsoleMenu('Bienvenid@ a la clínica veterinaria!', 'Selecciona una de las opciones a continuación.')
+    menu.append_item(SelectionItem('Ingresar al sistema',0))
+    menu.append_item(SelectionItem('Salir',1))
+    menu.show(False) # False evita que se muestra la opción de 'exit' que viene por defecto
 
-    while user_option not in ['1','2']:
-      user_option = input('Selecciona una de las opciones [1-2]: ')
-    
-    user_option = int(user_option) 
+    user_option = menu.selected_option
 
-    if user_option == 1:
+    if user_option == 0:
       # Se inicia el proceso de autenticación del usuario
       print('\n')
       user_rut = input('Ingresa tu RUT sin guión ni puntos: ')
@@ -129,7 +242,8 @@ class Client:
         # Se verifica el estado de la transacción ('OK' o 'NK') según el bus de servicios.
         if tx_status.lower() == 'nk':
           # Se ha producido un error en la transferencia de la transacción
-          print('[Error] Se ha producido un error en la transferencia de la transacción (NK).')
+          print(Style.RESET_ALL)
+          print(ERROR_STYLE+'[Error] Se ha producido un error en la transferencia de la transacción (NK).'+Style.RESET_ALL)
         
         else: # La transacción fue recibida correctamente
           # Se verifica si se pudo autenticar al usuario
@@ -137,28 +251,37 @@ class Client:
           if not user_data['auth_error']:
             # El usuario fue autenticado correctamente
             # Se muestra el siguiente menú interno con los otros servicios
-            print(user_data)
+            self.internal_menu_options(user_data)
           
           else:
             # Hubo un error al autentificar al usuario. Se muestra el mensaje de error enviado por el servicio.
-            print('[Error] El servicio de autenticación ha respondido con el siguiente error:')
-            print(user_data['error_notification'])
+            print(Style.RESET_ALL)
+            print(ERROR_STYLE+'[Error] El servicio de autenticación ha respondido con el siguiente error:')
+            print(user_data['error_notification']+Style.RESET_ALL)
       
       except Exception as error:
-        print('[Error] Se ha producido el siguiente error al procesar la transacción:')
-        print(str(error))
+        print(Style.RESET_ALL)
+        print(ERROR_STYLE+'[Error] Se ha producido el siguiente error al procesar la transacción:')
+        print(str(error)+Style.RESET_ALL)
 
-    elif user_option == 2:
-      print('\nHasta luego!')
+    elif user_option == 1:
+      print(Style.RESET_ALL)
+      print(INFO_STYLE+'Hasta luego!'+Style.RESET_ALL)
       self.sock.close()
       exit()
     
-    input('\nPresiona ENTER para continuar')
+    input(INSTRUCTIONS_STYLE+'\nPresiona ENTER para continuar'+Style.RESET_ALL)
 
   def run(self):
-    # Ciclo de ejecución del proceso cliente
     while True:
-      self.user_menu_options()
+      try:
+        # Ciclo de ejecución del proceso cliente
+        self.user_menu_options()
+      
+      except KeyboardInterrupt:
+        print(Style.RESET_ALL)
+        clear_screen()
+        exit()
 
 if __name__ == '__main__':
   # Configuración de argumentos solicitados al momento de ejecutar el comando en la terminal
@@ -175,6 +298,8 @@ if __name__ == '__main__':
   # Se obtienen los valores de host y puerto especificados en el comando de la terminal
   host = args.host
   port = args.port
+
+  clear_screen()
 
   # Se instancia el objeto de la clase cliente
   client = Client(host, port)
--- services/user_management.py
+++ services/user_management.py
@@ -2,7 +2,7 @@
 # pylint: enable=too-many-lines
 
 # ╔═════════════════════════════════════════════════════════════════════════════════════════════╗
-# ║ Servicio de autenticación de usuarios para proyecto de Arquitectura de Sistemas (02 - 2020) ║
+# ║ Servicio de gestión de usuarios para proyecto de Arquitectura de Sistemas (02 - 2020)       ║
 # ╠═════════════════════════════════════════════════════════════════════════════════════════════╣
 # ║ Integrantes:                                                                                ║
 # ║ * Lorenzo Alfaro Bravo                                                                      ║
@@ -13,7 +13,7 @@
 # Módulos a utilizar
 from db import db_wrapper
 from db.db_credentials import *
-import socket, argparse, bcrypt, os
+import socket, argparse, bcrypt, os, pickle
 from colorama import Back, Fore, Style, init
 
 # Inicialización para librería de colores
@@ -32,7 +32,7 @@ SUCCESS_STYLE = Back.GREEN + Fore.WHITE
 
 class Service:
   def __init__(self, host, port, name):
-    self.service_title = 'Servicio de autenticación de usuarios' # Título con descripción del servicio
+    self.service_title = 'Servicio de gestión de usuarios' # Título con descripción del servicio
     self.service_name = name # Nombre del servicio para reconocimiento del bus de servicios
 
     # Se realiza la conexión a la base de datos con las credenciales
@@ -56,7 +56,7 @@ class Service:
     except Exception as error:
       print(ERROR_STYLE+'[Error] Se ha producido el siguiente error al establecer conexión con el bus de servicios:')
       print(str(error)+Style.RESET_ALL)
-    
+  
   # Registro del nombre de servicio en el bus previo a la ejecución
   def bus_register(self):
     try:
@@ -74,7 +74,7 @@ class Service:
       print(ERROR_STYLE+'[Error] Se ha producido el siguiente error al registrar el servicio:')
       print(str(error)+Style.RESET_ALL)
       return
-
+  
   # Método para generar el largo de la transacción (servicio + data) - Ej: 18 (int) --> 00018 (str)
   def generate_tx_length(self, tx_length):
     char_ammount = 5 # Cantidad de caracteres máximos para definir el largo de la transacción (5 según formato solicitado por el bus)
@@ -99,7 +99,7 @@ class Service:
     tx_data = tx[10:] # Datos enviados por el cliente
 
     return (tx_length, tx_service, tx_data)
-
+  
   def run(self):
     # El servicio se mantiene escuchando a través del socket
     while True:
@@ -114,7 +114,7 @@ class Service:
         tx = tx.decode('UTF-8')
         # Se procesa la transacción para obtener los componentes individuales
         tx_length, tx_service, tx_data = self.split_tx(tx)
-        
+
         print('')
         print(INFO_STYLE+'Transacción recibida desde cliente'+Style.RESET_ALL)
         print(INSTRUCTIONS_STYLE+'\t- Largo de la transacción: ' +str(tx_length)+' ('+str(int(tx_length))+')'+Style.RESET_ALL)
@@ -125,36 +125,30 @@ class Service:
         try:
           client_data = eval(tx_data)
 
-          if client_data['tx_option'] == 1:
-            # Autenticación de cuenta
-            # Se revisa en la base de datos la existencia del usuario según el RUT y se obtiene el hash de la password si existe.
+          # Se verifica la opción de transacción enviada por el cliente
+          tx_option = client_data['tx_option']
+
+          if tx_option == 0:
+            # Se envía el menú interno al cliente
+            option_list = ['Ver lista de usuarios registrados', 'Ver detalle de usuario', 'Modificar usuario', 'Eliminar usuario', 'Volver']
+            resp_data = {}
+            resp_data['menu_title'] = 'Menú de gestión de usuarios'
+            resp_data['menu_subtitle'] = 'Selecciona una de las opciones a continuación.'
+            resp_data['menu_options'] = option_list
+          
+          elif tx_option == 1:
+            # Se envía la lista de usuarios registrados (ordenada por apellidos)
             sql_query = '''
-              SELECT *
+              SELECT rut,nombres,apellidos,email,direccion
                 FROM Usuarios
-                  WHERE rut = %s
+                  ORDER BY apellidos ASC
             '''
-            cursor = self.db.query(sql_query, (client_data['user_rut'],)) # Se ejecuta la consulta en la base de datos
-            user_reg = cursor.fetchone() # Se obtiene el registro único desde la base de datos
+            cursor = self.db.query(sql_query, None)
 
-            if user_reg is not None:
-              # El usuario se encuentra registrado en la base de datos
-              # Se comprueba si la contraseña enviada hace match con el hash almacenado en el registro del usuario
-              if bcrypt.checkpw(client_data['password'].encode(encoding='UTF-8'), user_reg['password'].encode(encoding='UTF-8')):
-                # La contraseña ingresada es correcta. Se envían los datos personales (omitiendo el hash de la password)
-                del user_reg['password']
-                resp_data = user_reg
-                resp_data['auth_error'] = False # Se agrega el flag de autenticación exitosa
-              
-              else:
-                # La contraseña ingresada es incorrecta
-                # # Se notifica con el error correspondiente
-                resp_data = {'auth_error': True, 'error_notification': 'Se ha producido un error de credenciales. Revisa nuevamente los campos.'}
+            # Se genera el objeto a enviar
+            resp_data = {}
+            resp_data['users_list'] = cursor.fetchall()
 
-            else:
-              # El usuario no se encuentra registrado en la base de datos
-              # Se notifica con el error correspondiente
-              resp_data = {'auth_error': True, 'error_notification': 'Se ha producido un error de credenciales. Revisa nuevamente los campos.'}
-        
         except Exception as error:
           print(ERROR_STYLE+error+Style.RESET_ALL)
           # Se genera el error y se envía al cliente
@@ -162,6 +156,7 @@ class Service:
         
         # Se genera la transacción y se envía al cliente
         tx = self.generate_tx(str(resp_data)).encode(encoding='UTF-8')
+
         self.sock.send(tx)
       
       except Exception as error:
@@ -170,7 +165,6 @@ class Service:
         print(str(error)+Style.RESET_ALL)
         error_msg = '[Error] Se ha producido el siguiente error al procesar la transacción:\n'+str(error)
         self.sock.send(self.generate_tx(error_msg).encode(encoding='UTF-8'))
-
 
 if __name__ == '__main__':
   # Configuración de argumentos solicitados al momento de ejecutar el comando en la terminal
